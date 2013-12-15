cee1dc0abe224f2f15e2ab89b3b9e8a4985bf302
vanzheng <vanzheng2012@gmail.com>
2013-12-15 10:55:52 +0800

    Update switchTabs

--- switchTabs.js
+++ switchTabs.js
@@ -1,22 +1,24 @@
 //contentList: the content list (jQuery object)
-//className: the current tab class name.
+//currentClass: the current tab class name.
 //eventType: switch tabs event. 
 (function ($) {
-    $.fn.switchTabs = function (options) {
-        var settings = $.extend({
-            contentList: null,
-            className: "",
-            eventType: "click"
-        }, options);
+    var defaults = {
+        contentList: null,
+        currentClass: '',
+        eventType: "click"
+    };
 
-        var tabs = $(this);
+    $.fn.switchTabs = function (options) {
+        var settings= $.extend({}, defaults, options);
+        var tabs = this;
         return this.each(function (index) {
-            $(this).bind(settings.eventType, function () {
-                tabs.filter("." + settings.className).removeClass(settings.className);
-                $(this).addClass(settings.className);
-                if (settings.list) {
-                    settings.list.filter(":visible").hide();
-                    settings.list.eq(index).show();
+            var _this = $(this);
+            _this.bind(settings.eventType, function () {
+                tabs.removeClass(settings.currentClass);
+                $(this).addClass(settings.currentClass);
+                if (settings.contentList) {
+                    settings.contentList.filter(":visible").hide();
+                    settings.contentList.eq(index).show();
                 }
             });
         });
